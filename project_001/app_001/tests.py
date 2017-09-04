27b66396e2dd08091945590d93d1bf2ba0236912
jiaxiaolei <jiaxiaolei@syswin.com>
2017-09-04 17:21:02 +0800

    feat: add tests.py for app

--- project_001/app_001/tests.py
+++ project_001/app_001/tests.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+from django.test import TestCase
+from django.test.client import Client
+
+
+class SendviewsTestCase(TestCase):
+
+    def test_http(self):
+        c = Client()
+        ret = c.post('/login/', {'username': 'john', 'password': 'smith'})
+        self.assertEqual(ret.status_code, 200)
+
+    def test_http2(self):
+        c = Client()
+        ret = c.get('/customer/details/')
+        self.assertEqual(ret.status_code, 200)
