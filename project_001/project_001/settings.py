966a062ca9d7a212dd2d36625fd1211c9ae2e8b4
jiaxiaolei <jiaxiaolei@syswin.com>
2017-09-15 11:07:41 +0800

    refactor: add rate in settings

--- project_001/project_001/settings.py
+++ project_001/project_001/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# 2.5%
+TAX_RATE = 0.025 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
